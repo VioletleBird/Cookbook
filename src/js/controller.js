eb45618f19316983863fb5f1a7749f83e23fbce4
Violet <hanfovab@gmail.com>
2023-09-28 20:20:38 +0200

    Implemented searching, adjusted views.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,17 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
 // https://forkify-api.herokuapp.com/v2
+
+//from parcel
+if(module.hot) {
+  module.hot.accept();
+};
 
 const controlRecipe = async function() {
   try {
@@ -25,7 +30,24 @@ const controlRecipe = async function() {
   }
 };
 
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner();
+    //get query
+    const query = searchView.getQuery();
+    if(!query) return;
+    //load search
+    await model.loadSearchResults(query);
+    //render results
+    resultsView.render(model.state.search.results);
+  }
+  catch (err) {
+    
+  }
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipe);
-}
+  searchView.addHandlerSearch(controlSearchResults);
+};
 init();
